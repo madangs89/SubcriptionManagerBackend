9a40afb2d285603153c59bfb350084a6133a6988
MADANA-GS <madangsnaik@gmail.com>
2026-03-02 21:51:35 +0530

    feat: update getMySubscription to handle async operations and return subscription details

--- src/controllers/subscrption.controller.ts
+++ src/controllers/subscrption.controller.ts
@@ -1,7 +1,8 @@
 import type { Response } from "express";
 import type { AuthMiddleWare } from "../interface/authMiddelware.interface.js";
+import Subscription from "../models/Subscription.js";
 
-export const getMySubScription = (req: AuthMiddleWare, res: Response) => {
+export const getMySubScription = async (req: AuthMiddleWare, res: Response) => {
   try {
     if (!req.user?._id) {
       return res.status(400).json({
@@ -9,6 +10,34 @@ export const getMySubScription = (req: AuthMiddleWare, res: Response) => {
         message: "User not found",
       });
     }
+    let userId = req.user._id;
+
+    const existingSub = await Subscription.findOne({
+      userId,
+      status: "success",
+    }).sort({ createdAt: -1 });
+
+    if (!existingSub) {
+      return res.status(404).json({
+        success: true,
+        message: "Subscription Not found",
+        subscription: null,
+      });
+    }
+
+    if (existingSub?.endDate && existingSub.endDate < new Date()) {
+      return res.status(400).json({
+        success: true,
+        message: "You don't have an active subscription",
+        subscription: null,
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "You have an active subscription",
+      subscription: existingSub,
+    });
   } catch (error) {
     return res
       .status(500)
