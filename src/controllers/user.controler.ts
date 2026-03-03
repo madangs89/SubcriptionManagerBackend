b55e90af1bee445db755c9cb7340c05614a74038
MADANA-GS <madangsnaik@gmail.com>
2026-03-03 18:00:02 +0530

    feat: implement user management functionalities for admin, including fetching, deleting, and subscription status

---
+++ src/controllers/user.controler.ts
@@ -0,0 +1,90 @@
+import type { Response, Request } from "express";
+import User from "../models/user.model.js";
+import Subscription from "../models/Subscription.js";
+
+export const getAllUsersForAdmin = async (req: Request, res: Response) => {
+  try {
+    const allUsers = await User.find()
+      .select("-password")
+      .sort({ createdAt: -1 });
+    res.status(200).json({
+      message: "Users fetched successfully",
+      success: true,
+      users: allUsers,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching users", error });
+  }
+};
+
+export const deleteUserByAdmin = async (
+  req: Request<{}, {}, { userId: string }>,
+  res: Response,
+) => {
+  try {
+    const { userId } = req.body;
+
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ message: "User ID is required", success: false });
+    }
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      return res
+        .status(404)
+        .json({ message: "User not found", success: false });
+    }
+    return res
+      .status(200)
+      .json({ message: "User deleted successfully", success: true });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error deleting user", success: false, error });
+  }
+};
+
+export const getAllSubscribedUsersForAdmin = async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    const allActiveSubscribedUsers = await Subscription.find({
+      status: "active",
+      endDate: { $gt: new Date() },
+    })
+      .populate("userId", "-password")
+      .sort({ createdAt: -1 });
+    return res.status(200).json({
+      message: "Subscribed users fetched successfully",
+      success: true,
+      users: allActiveSubscribedUsers,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching subscribed users", error });
+  }
+};
+
+export const getAllUnSubscribedUsersForAdmin = async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    const unsubscribedUsers = await Subscription.find({
+      status: "cancelled",
+    }).populate("userId", "-password");
+
+    return res.status(200).json({
+      message: "Unsubscribed users fetched successfully",
+      success: true,
+      users: unsubscribedUsers,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error fetching unsubscribed users", error });
+  }
+};
+
+
