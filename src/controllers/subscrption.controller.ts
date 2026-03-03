a8712ae71a7fa728dcc529dba4836e9529f2fdc3
MADANA-GS <madangsnaik@gmail.com>
2026-03-03 17:44:16 +0530

    feat: add subscription history retrieval, cancellation, and admin functionalities

--- src/controllers/subscrption.controller.ts
+++ src/controllers/subscrption.controller.ts
@@ -45,7 +45,118 @@ export const getMySubScription = async (req: AuthMiddleWare, res: Response) => {
   }
 };
 
+export const getAllSubScriptionHistory = async (
+  req: AuthMiddleWare,
+  res: Response,
+) => {
+  try {
+    if (!req.user?._id) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    let userId = req.user._id;
 
+    const allSubscriptions = await Subscription.find({ userId }).sort({
+      createdAt: -1,
+    });
 
+    return res.status(200).json({
+      success: true,
+      message: "All subscriptions retrieved successfully",
+      subscriptions: allSubscriptions,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
+};
 
+export const cancelSubscription = async (
+  req: AuthMiddleWare,
+  res: Response,
+) => {
+  try {
+    if (!req.user?._id) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    let userId = req.user._id;
 
+    const existingSub = await Subscription.findOne({
+      userId,
+      status: "active",
+    }).sort({ createdAt: -1 });
+    if (!existingSub) {
+      return res.status(404).json({
+        success: true,
+        message: "Active subscription Not found",
+      });
+    }
+    existingSub.status = "cancelled";
+    await existingSub.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Subscription cancelled successfully",
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
+};
+
+export const getAllSubScriptionForAdmin = async (
+  req: AuthMiddleWare,
+  res: Response,
+) => {
+  try {
+    const allSubscriptions = await Subscription.find().sort({
+      createdAt: -1,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "All subscriptions retrieved successfully",
+      subscriptions: allSubscriptions,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
+};
+
+export const getProfitForAdmin = async (req: AuthMiddleWare, res: Response) => {
+  try {
+    const allSubscriptions = await Subscription.find({
+      status: "success",
+    });
+    let totalAmount = 0;
+
+    if (allSubscriptions.length === 0) {
+      return res.status(200).json({
+        success: true,
+        message: "All subscriptions retrieved successfully",
+        totalAmount: 0,
+      });
+    }
+    for (let i = 0; i < allSubscriptions.length; i++) {
+      totalAmount += allSubscriptions[i]?.price || 0;
+    }
+    return res.status(200).json({
+      success: true,
+      message: "All subscriptions retrieved successfully",
+      totalAmount,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
+};
--- src/middlewares/authMiddelware.ts
+++ src/middlewares/authMiddelware.ts
@@ -4,6 +4,7 @@ import type { AuthMiddleWare } from "../interface/authMiddelware.interface.js";
 import jwt from "jsonwebtoken";
 import { config } from "../config/env.js";
 import type { JwtPayload } from "../types/auth.types.js";
+import Subscription from "../models/Subscription.js";
 export const authMiddelware = (
   req: AuthMiddleWare,
   res: Response,
@@ -20,6 +21,94 @@ export const authMiddelware = (
       token,
       config.JWT_SECRET_KEY as string,
     ) as JwtPayload;
+    req.user = decode;
+    next();
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({ message: error.message, success: false });
+    }
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
+};
+export const adminMiddelware = (
+  req: AuthMiddleWare,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized", success: false });
+    }
+
+    const decode = jwt.verify(
+      token,
+      config.JWT_SECRET_KEY as string,
+    ) as JwtPayload;
+
+    if (decode.role !== "admin") {
+      return res.status(403).json({ message: "Forbidden", success: false });
+    }
+    req.user = decode;
+    next();
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({ message: error.message, success: false });
+    }
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
+};
+export const subscriptionMiddelware = async (
+  req: AuthMiddleWare,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized", success: false });
+    }
+
+    const decode = jwt.verify(
+      token,
+      config.JWT_SECRET_KEY as string,
+    ) as JwtPayload;
+
+    const { _id } = decode;
+
+    if (!_id) {
+      return res
+        .status(400)
+        .json({ message: "User not found", success: false });
+    }
+
+    const existingSub = await Subscription.findOne({
+      userId: _id,
+      status: "success",
+    }).sort({ createdAt: -1 });
+
+    if (!existingSub) {
+      return res.status(404).json({
+        success: true,
+        message: "Subscription Not found",
+        subscription: null,
+      });
+    }
+
+    if (existingSub?.endDate && existingSub.endDate < new Date()) {
+      return res.status(400).json({
+        success: true,
+        message: "You don't have an active subscription",
+        subscription: null,
+      });
+    }
+
     req.user = decode;
     next();
   } catch (error) {
